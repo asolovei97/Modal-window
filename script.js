320f133289926bad069eab351b795eb54f7ea0eb
asolovei97 <01slick_coup@icloud.com>
2024-08-07 17:17:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+const closeModal = () => {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
